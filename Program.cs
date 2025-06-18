8a62d56ba882f06ba0414d3cec999ddd48879c95
S4m43l16 <jeanpaoloramos@gmail.com>
2025-06-18 15:27:43 -0400

    Primer commit, inicio del desarrollo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace IISecFramework;
+
+public class Framework
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hola");
+    }
+ }
