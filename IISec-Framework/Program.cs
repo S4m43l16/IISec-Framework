08f0b28fe6d5bb1850c02bb9a6f7b0c9cabbcba3
S4m43l <jeanpaoloramos@gmail.com>
2026-04-23 15:59:02 -0400

    Update

--- IISec-Framework/Program.cs
+++ IISec-Framework/Program.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Collections.Generic;
 
 using IISec_Framework.SearchWebConfig;
-using IISec_Framework.DataMake;
+
 
 namespace IISec_Framework;
 
@@ -23,24 +23,10 @@ public class Framework
                 choice = ".";
             }
             Console.WriteLine("Se ha encontrado un archivo web.config en la ruta indicada. \nMostrando contenido:");
+        }
 
-            for (int i = 0; i < dataProcess.Readfile(choice).Count; i++)
-            {
-                Console.WriteLine(dataProcess.Readfile(choice)[i]);
-            }
-            Console.WriteLine("\n¿Desea sobre escribirlo?");
-            string response = Console.ReadLine();
-
-            if (response.ToLower() == "si" || response.ToLower() == "s")
-            {
-                // llamar a la funcion que sobreescribe el archivo web.config
-                // Aun falta crear la clase, el metodo y la logica para sobreescribir el archivo
-                Console.WriteLine("El archivo web.config ha sido sobrescrito.");
-            }
-            else
-            {
-                Console.WriteLine("No se ha realizado ningun cambio en el archivo web.config.");
-            }
+        else if(WebConfigSearch.Search(choice) == false){
+            
         }
         else
         {
